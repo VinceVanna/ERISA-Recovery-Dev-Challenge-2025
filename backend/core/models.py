a6755c28c6314b956ef83e789c47852110bb9b53
VinceVanna <thfmercury@gmail.com>
2025-08-18 16:16:22 -0500

    Created ClaimList Model & Django Admin

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ClaimList(models.Model):
+    patient_name = models.CharField(max_length=100)
+    billed_amount = models.DecimalField(max_digits=20, decimal_places=2)
+    paid_amount = models.DecimalField(max_digits=20, decimal_places=2)
+    status = models.CharField(max_length=100)
+    insurer_name = models.CharField(max_length=100)
+    discharge_date = models.DateField()
+
+    def __str__(self):
+        return f"ID: {self.id} - Patient Name: {self.patient_name}"
