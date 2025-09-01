b77f9ab6b7c137b2a5a253a35a153ccf5e722176
VinceVanna <thfmercury@gmail.com>
2025-09-01 00:15:49 -0500

    created models for Employee, Notes, Annotations, and Flags

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import ClaimList, ClaimDetail
+from .models import ClaimList, ClaimDetail, Employee, Note, Annotation, Flag
 # Register your models here.
 
 admin.site.register(ClaimList)
 admin.site.register(ClaimDetail)
+admin.site.register(Employee)
+admin.site.register(Note)
+admin.site.register(Annotation)
+admin.site.register(Flag)
--- backend/core/models.py
+++ backend/core/models.py
@@ -19,3 +19,47 @@ class ClaimDetail(models.Model):
 
     def __str__(self):
         return f"ID: {self.id} - Claim ID: {self.claim_id}"
+    
+class Employee(models.Model):
+    employee_first_name = models.CharField(max_length=255)
+    employee_last_name = models.CharField(max_length=255)
+    employee_password = models.CharField(max_length=255, null=True, blank=True)
+    employee_type = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id} - Name: {self.employee_first_name} {self.employee_last_name} - Employee Type: {self.employee_type}"
+    
+class Note(models.Model):
+    claim_id = models.ForeignKey(ClaimList, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id} - Claim ID: {self.claim_id} - User ID: {self.claim_id}"
+    
+class Annotation(models.Model):
+    claim_id = models.ForeignKey(ClaimList, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id} - Claim ID: {self.claim_id} - User ID: {self.claim_id}"
+    
+class Flag(models.Model):
+    claim_id = models.ForeignKey(ClaimList, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    flag = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('claim_id', 'user_id')
+
+    def __str__(self):
+        return f"ID: {self.id} - Claim ID: {self.claim_id} - User ID: {self.claim_id}"
