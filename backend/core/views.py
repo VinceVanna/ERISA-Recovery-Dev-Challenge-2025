93aa24611b46df0758a5ac969c304c33f3c492ec
VinceVanna <thfmercury@gmail.com>
2025-09-04 00:43:53 -0500

    created functional search bar and updated table display

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import ClaimList, ClaimDetail
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -10,7 +11,10 @@ def hello(request):
     return HttpResponse("<p>Hello from HTMX!</p>")
 
 def load_claim_list(request):
-    get_all_claims = ClaimList.objects.all()
+    query = request.GET.get('search','')
+    get_all_claims = ClaimList.objects.filter(
+        Q(id__icontains=query) | Q(patient_name__icontains=query)
+    )
     return render(request, 'core/claim_list_page/claim_list_table.html', {'claims': get_all_claims})
 
 def display_claim_list(request):
@@ -24,5 +28,13 @@ def display_claim_detail(request):
     return render(request, 'core/claim_list_page/claim_detail.html')
 
 def search_claim_detail(request, claim_id):
+    get_claim = get_object_or_404(ClaimList, id=claim_id)
     get_detail = get_object_or_404(ClaimDetail, claim_id__id=claim_id)
-    return render(request, 'core/claim_list_page/claim_detail_tab.html', {'detail': get_detail})
+    context = {
+        'claim': get_claim,
+        'detail': get_detail
+    }
+    return render(request, 'core/claim_list_page/claim_detail_tab.html', context)
+
+def load_navbar(request):
+    return render(request, 'core/navbar.html')
